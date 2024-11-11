66766b8b03760c576c081b8e8aa98aad9275fc6e
duanjie-cmd <18870420982@139.com>
2024-11-11 14:55:21 +0800

    add chat-id

--- src/helpers/getAgentUrls.ts
+++ src/helpers/getAgentUrls.ts
@@ -1,11 +1,11 @@
 import type { InferenceAgent } from "../codec/agent/v1/agent";
 
-export const getAgentUrls = (selectAgent: InferenceAgent) => {
+export const getAgentUrls = (selectAgent: InferenceAgent,chatId:string) => {
   const baseUrl = `${selectAgent.url}${
     selectAgent.url?.endsWith("/") ? "" : "/"
   }`;
   return {
-    agentWsUrl: `${baseUrl}chat`,
-    agentHeartbeatUrl: `${baseUrl}heartbeat`,
+    agentWsUrl: `${baseUrl}chat?chat-id=${chatId}`,
+    agentHeartbeatUrl: `${baseUrl}heartbeat?chat-id=${chatId}`,
   };
 };
